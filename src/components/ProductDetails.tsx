25c1a8ae5509679e03fb4403445d0c631ff4674c
RuwandieD <dharmapalard.21@uom.lk>
2024-11-27 17:54:45 +0530

    Single product view basic completed.

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,65 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 import { HeartIcon } from "@heroicons/react/24/outline";
+import { Tab } from "@headlessui/react";
 import { Product } from "@/interfaces/productInterface";
 
 const ProductDetails = ({ product }: { product: Product }) => {
+    const [quantity, setQuantity] = useState(1);
+
+    const handleQuantityChange = (type: "increment" | "decrement") => {
+        if (type === "increment" && quantity < product.stock) {
+            setQuantity((prev) => prev + 1);
+        }
+        if (type === "decrement" && quantity > 1) {
+            setQuantity((prev) => prev - 1);
+        }
+    };
+
     return (
-        <div className="bg-light-green text-black min-h-screen font-sans">
-            <div className="mx-auto max-w-4xl px-4 py-8">
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-8">
-                    {/* Image Gallery */}
-                    <div>
-                        {product.images.length > 0 ? (
-                            <img
-                                src={product.images[0].url}
-                                alt={`Image for product ${product.name}`}
-                                className="rounded-lg overflow-hidden w-full h-auto object-cover border border-black"
-                            />
-                        ) : (
-                            <p className="text-gray-500">No images available</p>
-                        )}
+        <div className="bg-gray-100 text-black min-h-screen font-sans">
+            <div className="mx-auto max-w-5xl px-4 py-8">
+                <div className="bg-white shadow-lg rounded-lg p-8">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-8">
+                        {/* Image Gallery */}
+                        <div>
+                            <Tab.Group>
+                                <Tab.Panels>
+                                    {product.images.map((image, index) => (
+                                        <Tab.Panel key={index} className="rounded-lg overflow-hidden">
+                                            <img
+                                                src={image.url}
+                                                alt={`Image ${index}`}
+                                                className="w-full h-auto object-cover"
+                                            />
+                                        </Tab.Panel>
+                                    ))}
+                                </Tab.Panels>
+                                <Tab.List className="flex space-x-2 mt-4">
+                                    {product.images.map((image, index) => (
+                                        <Tab key={index} className="w-16 h-16 border rounded-lg">
+                                            {({ selected }) => (
+                                                <img
+                                                    src={image.url}
+                                                    alt={`Thumbnail ${index}`}
+                                                    className={`object-cover w-full h-full ${
+                                                        selected ? "ring-2 ring-black" : ""
+                                                    }`}
+                                                />
+                                            )}
+                                        </Tab>
+                                    ))}
+                                </Tab.List>
+                            </Tab.Group>
+                        </div>
+
+                        {/* Product Info */}
+                        <div className="px-4">
+                            <h1 className="text-3xl font-bold">{product.name}</h1>
+                            <p className="text-2xl mt-3">{`Price: $${product.price}`}</p>
+                            <p className="text-gray-600 mt-2">{product.description}</p>
+
+                            {/* Quantity Selector */}
+                            <div className="mt-6">
+                                <h3 className="text-sm font-bold">Quantity</h3>
+                                <div className="flex items-center space-x-2 mt-2">
+                                    <button
+                                        onClick={() => handleQuantityChange("decrement")}
+                                        disabled={quantity === 1}
+                                        className="px-3 py-1 border border-gray-300 bg-white rounded text-gray-600 hover:bg-gray-100 disabled:opacity-50"
+                                    >
+                                        -
+                                    </button>
+                                    <span className="px-4 py-1 border border-gray-300 bg-gray-100 rounded">
+                                        {quantity}
+                                    </span>
+                                    <button
+                                        onClick={() => handleQuantityChange("increment")}
+                                        disabled={quantity === product.stock}
+                                        className="px-3 py-1 border border-gray-300 bg-white rounded text-gray-600 hover:bg-gray-100 disabled:opacity-50"
+                                    >
+                                        +
+                                    </button>
+                                </div>
+                            </div>
+
+                            {/* Add to Cart */}
+                            <div className="mt-6 flex">
+                                <button
+                                    type="submit"
+                                    className="flex max-w-xs flex-1 items-center justify-center rounded-md bg-black px-8 py-3 text-base font-medium text-white hover:bg-gray-700"
+                                >
+                                    Add to cart
+                                </button>
+                                <button
+                                    type="button"
+                                    className="ml-4 flex items-center justify-center rounded-md px-3 py-3 text-gray-400 hover:bg-gray-100"
+                                >
+                                    <HeartIcon className="h-6 w-6 text-gray-400 hover:text-red-500" />
+                                    <span className="sr-only">Add to favorites</span>
+                                </button>
+                            </div>
+
+                            {/* Product Type */}
+                            <div className="mt-6">
+                                <h3 className="text-sm font-bold">Product Type</h3>
+                                <p className="mt-2">{product.product_type.name}</p>
+                            </div>
+                        </div>
                     </div>
 
-                    {/* Product Info */}
-                    <div className="mt-10 px-4 lg:mt-0">
-                        <h1 className="text-3xl font-bold tracking-tight">{product.name}</h1>
-                        <div className="mt-3">
-                            <p className="text-3xl tracking-tight">{`Price: $${product.price}`}</p>
-                        </div>
-
-                        <div className="mt-6">
-                            <h3 className="text-sm">Description</h3>
-                            <p className="mt-2 text-base">{product.description}</p>
-                        </div>
-
-                        <div className="mt-6">
-                            <h3 className="text-sm">Stock</h3>
-                            <p className="mt-2 text-base">{product.stock}</p>
-                        </div>
-
-                        {/* Add to Cart */}
-                        <div className="mt-6 flex">
-                            <button
-                                type="submit"
-                                className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-black px-8 py-3 text-base font-medium text-white hover:!bg-gray-500 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2"
-                            >
-                                Add to cart
-                            </button>
-
-                            <button
-                                type="button"
-                                className="ml-4 flex items-center justify-center rounded-md px-3 py-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500"
-                            >
-                                <HeartIcon className="h-6 w-6 flex-shrink-0 text-gray-400 hover:text-green" />
-                                <span className="sr-only">Add to favorites</span>
-                            </button>
-                        </div>
-
-                        {/* Product Type */}
-                        <div className="mt-6">
-                            <h3 className="text-sm">Product Type</h3>
-                            <p className="mt-2 text-base">{product.product_type.name}</p>
-                        </div>
+                    {/* Detailed Description */}
+                    <div className="mt-10">
+                        <h2 className="text-xl font-bold">Product Details</h2>
+                        <p className="mt-4 text-gray-700 text-base">
+                            {product.description}
+                        </p>
                     </div>
                 </div>
             </div>
--- src/pages/products/[product_id].tsx
+++ src/pages/products/[product_id].tsx
@@ -46,72 +46,7 @@ const ProductPage = () => {
 
     return (
         <VisitorLayout>
-            <div className="bg-gray-100 min-h-screen font-sans flex flex-col items-center pt-10">
-                <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-4xl">
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-8">
-                        {/* Image Gallery */}
-                        {product?.images?.length > 0 ? (
-                            <div>
-                                <h2 className="text-xl font-bold mb-4">Images</h2>
-                                {product.images.map((image: any, index: number) => (
-                                    <div
-                                        key={index}
-                                        className="rounded-lg overflow-hidden mb-4 border border-gray-300"
-                                    >
-                                        <img
-                                            src={image.url}
-                                            alt={image.alt || `Image ${index}`}
-                                            className="w-full h-auto object-cover"
-                                        />
-                                    </div>
-                                ))}
-                            </div>
-                        ) : (
-                            <p className="text-gray-500">No images available</p>
-                        )}
-
-                        {/* Product Info */}
-                        <div className="px-4 lg:px-0">
-                            <h1 className="text-3xl font-bold tracking-tight text-black">
-                                {product.name}
-                            </h1>
-                            <p className="text-2xl mt-3 text-black">{`Price: $${product.price}`}</p>
-                            <p className="text-gray-600 mt-2">{product.description}</p>
-                            <p className="text-gray-800 mt-2">{`Stock: ${product.stock}`}</p>
-
-                            {/* Add to Cart */}
-                            <div className="mt-6 flex">
-                                <button
-                                    type="button"
-                                    className="flex items-center justify-center w-full rounded-md bg-black px-6 py-3 text-white hover:bg-gray-800"
-                                >
-                                    Add to cart
-                                </button>
-                                <button
-                                    type="button"
-                                    className="ml-4 flex items-center justify-center rounded-md px-3 py-3 text-gray-400 hover:bg-light-green"
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1.5}
-                                        stroke="currentColor"
-                                        className="w-6 h-6"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M16.5 3.75a6.375 6.375 0 00-9 9L12 18l4.5-5.25a6.375 6.375 0 000-9z"
-                                        />
-                                    </svg>
-                                    <span className="sr-only">Add to favorites</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <ProductDetails product={product} />
         </VisitorLayout>
     );
 };
